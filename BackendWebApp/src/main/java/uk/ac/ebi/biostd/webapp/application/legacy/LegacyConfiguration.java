5e92ed6e7513ef29ee68bbb9fce1585afdf2c7f1
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2018-02-08 14:58:33 +0000

    Pivotal-155033113: Add page tab format submission test  - code review changes

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/legacy/LegacyConfiguration.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/legacy/LegacyConfiguration.java
@@ -3,11 +3,20 @@ package uk.ac.ebi.biostd.webapp.application.legacy;
 import javax.servlet.ServletContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.env.Environment;
+import uk.ac.ebi.biostd.webapp.server.config.BackendConfig;
 import uk.ac.ebi.biostd.webapp.server.config.ConfigurationManager;
+import uk.ac.ebi.biostd.webapp.server.mng.SessionManager;
 
 @Configuration
 public class LegacyConfiguration {
+
+    @Bean
+    @DependsOn("appInitializer")
+    public SessionManager serviceManager() {
+        return BackendConfig.getServiceManager().getSessionManager();
+    }
 
     @Bean
     public ConfigurationManager configurationManager(ServletContext context, Environment environment) {
