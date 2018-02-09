8898528658d3603b8771508380856dba672980a0
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2018-02-09 11:55:30 +0000

    Pivotal-155067936: Add Json submission integration test  - code review changes

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/server/endpoint/ServiceServlet.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/server/endpoint/ServiceServlet.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2014-2017 Functional Genomics Development Team, European Bioinformatics Institute
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * @author Mikhail Gostev <gostev@gmail.com>
- **/
-
 package uk.ac.ebi.biostd.webapp.server.endpoint;
 
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
@@ -52,10 +37,6 @@ public abstract class ServiceServlet extends HttpServlet {
     protected boolean isAntonymous(Session session) {
         return session == null || session.isAnonymouns();
     }
-
-
-
-
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
