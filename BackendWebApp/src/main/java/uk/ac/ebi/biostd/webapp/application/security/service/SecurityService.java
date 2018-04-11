191fe4ad3c663050ec06753689c2052869979061
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2018-04-11 13:49:32 +0100

    fixed security

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/security/service/SecurityService.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/security/service/SecurityService.java
@@ -170,16 +170,15 @@ public class SecurityService implements ISecurityService {
         boolean isAuthor = submission.getOwnerId() == userId;
         boolean isPublic = isPublicSubmission(submission.getAccessTag());
         boolean isSuperUser = user.isSuperuser();
-        boolean hasAccessTag = permissionsRepository
-                .existsByAccessTagInAndAccessType(submission.getAccessTag(), accessType);
+        boolean hasTag = permissionsRepository.existsByAccessTagInAndAccessType(submission.getAccessTag(), accessType);
 
         switch (accessType) {
             case READ:
-                return isPublic || isAuthor;
+                return isPublic || isAuthor || hasTag || isSuperUser;
             case SUBMIT:
                 return (isPublic && isAuthor) || isSuperUser;
             case ATTACH:
-                return hasAccessTag || isSuperUser;
+                return hasTag || isSuperUser;
             case UPDATE:
             case DELETE:
                 return isAuthor || isSuperUser;
