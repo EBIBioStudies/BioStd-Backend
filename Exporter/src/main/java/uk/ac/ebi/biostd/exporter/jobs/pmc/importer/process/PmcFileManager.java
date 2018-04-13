55ede85dc98a50e00fc6c127cb4291d88c884922
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2018-04-13 08:58:12 +0100

    refactor controllers

--- Exporter/src/main/java/uk/ac/ebi/biostd/exporter/jobs/pmc/importer/process/PmcFileManager.java
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/jobs/pmc/importer/process/PmcFileManager.java
@@ -7,20 +7,25 @@ import java.nio.file.Paths;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.biostd.exporter.jobs.pmc.importer.PmcImportProperties;
 import uk.ac.ebi.biostd.in.pagetab.SubmissionInfo;
 
 @Slf4j
-@AllArgsConstructor
+@Component
 public class PmcFileManager {
 
     private final static String DOWNLOAD_URL = "http://europepmc.org/articles/%s/bin/%s";
     private final ExecutorService executor = Executors.newFixedThreadPool(10);
 
     private final String userPath;
+
+    public PmcFileManager(PmcImportProperties properties) {
+        this.userPath = properties.getSubmitterUserPath();
+    }
 
     @SneakyThrows
     public void downloadFiles(SubmissionInfo submissionInfo) {
---
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/rest/TasksControllers.java
@@ -0,0 +1,52 @@
+package uk.ac.ebi.biostd.exporter.rest;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ebi.biostd.exporter.jobs.common.api.ExportPipeline;
+import uk.ac.ebi.biostd.exporter.jobs.partial.PartialSubmissionExporter;
+import uk.ac.ebi.biostd.exporter.jobs.pmc.importer.PmcImporter;
+
+@RestController
+public class TasksControllers {
+
+    private final ExportPipeline fullExporter;
+    private final ExportPipeline pmcExporter;
+    private final PartialSubmissionExporter partialExporter;
+    private final PmcImporter pmcImporter;
+
+    public TasksControllers(
+            @Qualifier("full") ExportPipeline fullExporter,
+            @Qualifier("pmc") ExportPipeline pmcExporter,
+            PartialSubmissionExporter partialExporter,
+            PmcImporter pmcImporter) {
+        this.fullExporter = fullExporter;
+        this.pmcExporter = pmcExporter;
+        this.partialExporter = partialExporter;
+        this.pmcImporter = pmcImporter;
+    }
+
+    @GetMapping("/api/force/full")
+    public String forceFull() {
+        fullExporter.execute();
+        return "ok";
+    }
+
+    @GetMapping("/api/force/partial")
+    public String forcePartial() {
+        partialExporter.execute();
+        return "ok";
+    }
+
+    @GetMapping("/api/force/pmc")
+    public String pmcExport() {
+        pmcExporter.execute();
+        return "ok";
+    }
+
+    @GetMapping("/api/force/pmc")
+    public String pmcImporter() {
+        pmcImporter.execute();
+        return "ok";
+    }
+}
