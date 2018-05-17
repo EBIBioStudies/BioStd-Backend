d62a0272172270e36758bad97d2bc0482c25d085
olgamelnichuk <olkin@ebi.ac.uk>
2018-05-17 15:29:47 +0100

    [157597056] Set default values to 'offset' and 'limit' fields in the pending submission list filters

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/rest/dto/PendingSubmissionListFiltersDto.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/rest/dto/PendingSubmissionListFiltersDto.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class PendingSubmissionListFiltersDto {
-    private final Integer offset;
-    private final Integer limit;
+    private final Integer offset = 0;
+    private final Integer limit = 50;
     private final String accNo;
     private final Long rTimeFrom;
     private final Long rTimeTo;
