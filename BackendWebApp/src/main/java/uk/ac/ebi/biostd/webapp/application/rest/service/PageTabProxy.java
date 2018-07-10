da19360efcb71c0800f0f4d46c77d4b8520fd85b
olgamelnichuk <olkin@ebi.ac.uk>
2018-07-10 16:50:06 +0100

    [157597056] added PageTabProxy unit tests

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/rest/service/PageTabProxy.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/rest/service/PageTabProxy.java
@@ -2,17 +2,127 @@ package uk.ac.ebi.biostd.webapp.application.rest.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.mysql.cj.xdevapi.JsonArray;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-import javax.swing.text.html.Option;
 
 public class PageTabProxy {
+
+    private interface InnerProxy {
+        Optional<String> getTextField(String name);
+
+        void setTextField(String name, String value);
+
+        Set<String> getAttribute(String name);
+
+        void setAttribute(String name, Set<String> values, ObjectMapper objectMapper);
+
+        JsonNode pageTab();
+    }
+
+    private static class InnerProxyImpl implements InnerProxy {
+
+        private JsonNode pageTab;
+
+        private InnerProxyImpl(JsonNode pageTab) {
+            this.pageTab = pageTab;
+        }
+
+        @Override
+        public Optional<String> getTextField(String propertyName) {
+            return getNodeTextField(pageTab, propertyName);
+        }
+
+        @Override
+        public void setTextField(String propertyName, String propertyValue) {
+            ((ObjectNode) pageTab).put(propertyName, propertyValue);
+        }
+
+        @Override
+        public Set<String> getAttribute(String attrName) {
+            return getAttributes(pageTab)
+                    .stream()
+                    .filter(attrNameFilter(attrName))
+                    .map(this::getAttrValue)
+                    .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
+                    .collect(Collectors.toSet());
+        }
+
+        @Override
+        public void setAttribute(String attrName, Set<String> values, ObjectMapper objectMapper) {
+            List<JsonNode> attrNodes = Stream.concat(
+                    getAttributes(pageTab)
+                            .stream()
+                            .filter(attrNameFilter(attrName).negate()),
+                    values.stream().map(value ->
+                            objectMapper.createObjectNode().put(NAME_FIELD, attrName).put(VALUE_FIELD, value)
+                    )).collect(Collectors.toList());
+
+            ((ObjectNode) pageTab).set(ATTRIBUTES_FIELD, objectMapper.createArrayNode().addAll(attrNodes));
+        }
+
+        private List<JsonNode> getAttributes(JsonNode node) {
+            List<JsonNode> list = new ArrayList<>();
+            final String attributesProp = ATTRIBUTES_FIELD;
+            if (node.has(attributesProp)) {
+                JsonNode attributesNode = node.get(attributesProp);
+                if (attributesNode.isArray()) {
+                    attributesNode.iterator().forEachRemaining(list::add);
+                }
+            }
+            // NB: only submission node has 'section' property
+            Optional.ofNullable(node.get(SECTION_FIELD)).ifPresent(sectionNode -> {
+                list.addAll(getAttributes(sectionNode));
+            });
+            return list;
+        }
+
+        private Predicate<JsonNode> attrNameFilter(String attrName) {
+            return jsonNode -> getNodeTextField(jsonNode, NAME_FIELD).orElse("").equalsIgnoreCase(attrName);
+        }
+
+        private Optional<String> getAttrValue(JsonNode jsonNode) {
+            return getNodeTextField(jsonNode, VALUE_FIELD);
+        }
+
+        private Optional<String> getNodeTextField(JsonNode node, String fieldName) {
+            return Optional.ofNullable(node.get(fieldName)).map(JsonNode::asText).map(String::trim);
+        }
+
+        @Override
+        public JsonNode pageTab() {
+            return pageTab;
+        }
+    }
+
+    private static final InnerProxy EMPTY_INNER_PROXY = new InnerProxy() {
+        @Override
+        public Optional<String> getTextField(String name) {
+            return Optional.empty();
+        }
+
+        @Override
+        public void setTextField(String name, String value) {
+        }
+
+        @Override
+        public Set<String> getAttribute(String name) {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public void setAttribute(String name, Set<String> values, ObjectMapper objectMapper) {
+        }
+
+        @Override
+        public JsonNode pageTab() {
+            return null;
+        }
+    };
+
     private static String ACCNO_FIELD = "accno";
     private static String NAME_FIELD = "name";
     private static String VALUE_FIELD = "value";
@@ -22,36 +132,36 @@ public class PageTabProxy {
     private static String RELEASE_DATE_ATTRIBUTE = "ReleaseDate";
     private static String ATTACH_TO_ATTRIBUTE = "AttachTo";
 
-    private JsonNode root;
-    private JsonNode pageTab;
+    private final JsonNode root;
+    private final InnerProxy innerProxy;
 
-    public PageTabProxy(JsonNode root) {
-        this.root = root;
+    public PageTabProxy(JsonNode node) {
+        this.root = node;
 
-        Iterable<Map.Entry<String, JsonNode>> iterable = () -> root.fields();
+        Optional<JsonNode> opNode = Optional.ofNullable(node);
+        Iterable<Map.Entry<String, JsonNode>> iterable = () -> opNode.map(JsonNode::fields).orElse(Collections.emptyIterator());
 
         Optional<JsonNode> submissions = StreamSupport.stream(iterable.spliterator(), false)
                 .filter(field -> field.getKey().equalsIgnoreCase("submissions"))
                 .findFirst()
                 .map(Map.Entry::getValue);
 
-        if (submissions.isPresent()) {
-            pageTab = submissions.filter(arrayNode -> arrayNode.has(0))
-                    .map(arrayNode -> arrayNode.get(0)).orElse(null);
-        } else {
-            pageTab = root;
-        }
+        JsonNode ptNode = submissions.isPresent() ?
+                submissions
+                        .filter(arrayNode -> arrayNode.has(0))
+                        .map(arrayNode -> arrayNode.get(0))
+                        .orElse(null) : node;
+
+        innerProxy = createInnerProxy(ptNode);
     }
 
     public Optional<String> getAccno() {
-        return Optional.ofNullable(pageTab)
-                .flatMap(pt -> getTextField(pt, ACCNO_FIELD))
+        return innerProxy.getTextField(ACCNO_FIELD)
                 .filter(v -> !v.isEmpty());
     }
 
     public PageTabProxy setAccno(String value) {
-        Optional.ofNullable(pageTab)
-                .ifPresent(pt -> setTextField(pt, ACCNO_FIELD, value));
+        innerProxy.setTextField(ACCNO_FIELD, value);
         return this;
     }
 
@@ -60,26 +170,19 @@ public class PageTabProxy {
     }
 
     public Optional<String> getTitle() {
-        return Optional.ofNullable(pageTab)
-                .map(pt -> getAttribute(pt, TITLE_ATTRIBUTE))
-                .flatMap(set -> set.stream().findFirst());
+        return innerProxy.getAttribute(TITLE_ATTRIBUTE).stream().findFirst();
     }
 
     public Optional<String> getReleaseDate() {
-        return Optional.ofNullable(pageTab)
-                .map(pt -> getAttribute(pt, RELEASE_DATE_ATTRIBUTE))
-                .flatMap(set -> set.stream().findFirst());
+        return innerProxy.getAttribute(RELEASE_DATE_ATTRIBUTE).stream().findFirst();
     }
 
     public Set<String> getAttachToAttr() {
-        return Optional.ofNullable(pageTab)
-                .map(pt -> getAttribute(pt, ATTACH_TO_ATTRIBUTE))
-                .orElse(Collections.emptySet());
+        return innerProxy.getAttribute(ATTACH_TO_ATTRIBUTE);
     }
 
     public PageTabProxy setAttachToAttr(Set<String> values, ObjectMapper objectMapper) {
-        Optional.ofNullable(pageTab)
-                .ifPresent(pt -> setAttribute(pt, ATTACH_TO_ATTRIBUTE, values, objectMapper));
+        innerProxy.setAttribute(ATTACH_TO_ATTRIBUTE, values, objectMapper);
         return this;
     }
 
@@ -94,64 +197,17 @@ public class PageTabProxy {
     }
 
     public JsonNode wrappedJson(ObjectMapper objectMapper) {
-        if (root != pageTab) {
+        if (root != innerProxy.pageTab()) {
             return root;
         }
 
         return objectMapper.createObjectNode()
-                .set("submissions", objectMapper.createArrayNode().add(pageTab));
+                .set("submissions", objectMapper.createArrayNode().add(innerProxy.pageTab()));
     }
 
-    private Optional<String> getTextField(JsonNode node, String propertyName) {
-        return Optional.ofNullable(node.get(propertyName)).map(JsonNode::asText).map(String::trim);
-    }
-
-    private void setTextField(JsonNode node, String propertyName, String propertyValue) {
-        ((ObjectNode) node).put(propertyName, propertyValue);
-    }
-
-    private Set<String> getAttribute(JsonNode node, String attrName) {
-        return getAttributes(node)
-                .stream()
-                .filter(attrNameFilter(attrName))
-                .map(this::getAttrValue)
-                .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
-                .collect(Collectors.toSet());
-    }
-
-    private void setAttribute(JsonNode node, String attrName, Set<String> values, ObjectMapper objectMapper) {
-        List<JsonNode> attrNodes = Stream.concat(
-                getAttributes(node)
-                        .stream()
-                        .filter(attrNameFilter(attrName).negate()),
-                values.stream().map(value ->
-                        objectMapper.createObjectNode().put(NAME_FIELD, attrName).put(VALUE_FIELD, value)
-                )).collect(Collectors.toList());
-
-        ((ObjectNode) node).set(ATTRIBUTES_FIELD, objectMapper.createArrayNode().addAll(attrNodes));
-    }
-
-    private List<JsonNode> getAttributes(JsonNode node) {
-        List<JsonNode> list = new ArrayList<>();
-        final String attributesProp = ATTRIBUTES_FIELD;
-        if (node.has(attributesProp)) {
-            JsonNode attributesNode = node.get(attributesProp);
-            if (attributesNode.isArray()) {
-                attributesNode.iterator().forEachRemaining(list::add);
-            }
-        }
-        // NB: only submission node has 'section' property
-        Optional.ofNullable(node.get(SECTION_FIELD)).ifPresent(sectionNode -> {
-            list.addAll(getAttributes(sectionNode));
-        });
-        return list;
-    }
-
-    private Predicate<JsonNode> attrNameFilter(String attrName) {
-        return jsonNode -> getTextField(jsonNode, NAME_FIELD).orElse("").equalsIgnoreCase(attrName);
-    }
-
-    private Optional<String> getAttrValue(JsonNode jsonNode) {
-        return getTextField(jsonNode, VALUE_FIELD);
+    private static InnerProxy createInnerProxy(JsonNode node) {
+        return Optional.ofNullable(node)
+                .map(n -> (InnerProxy) new InnerProxyImpl(n))
+                .orElse(EMPTY_INNER_PROXY);
     }
 }
