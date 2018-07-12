4f1830318e1eeb4073da324f07799f3bfac5b282
olgamelnichuk <olkin@ebi.ac.uk>
2018-07-12 11:39:32 +0100

    [157597056] refactoring: now with @AllArgsConstructor

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/rest/dto/ApiErrorDto.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/rest/dto/ApiErrorDto.java
@@ -1,13 +1,10 @@
 package uk.ac.ebi.biostd.webapp.application.rest.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ApiErrorDto {
-
     private String message;
-
-    public ApiErrorDto(String message) {
-        this.message = message;
-    }
 }
