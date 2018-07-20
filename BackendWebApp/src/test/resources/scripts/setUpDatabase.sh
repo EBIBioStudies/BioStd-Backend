630feb21f054eb0387d7d0b2d653879e4cbc29b7
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-07-20 16:31:03 +0100

    Remove docker compose, use docker run directly

--- BackendWebApp/src/test/resources/scripts/setUpDatabase.sh
+++ BackendWebApp/src/test/resources/scripts/setUpDatabase.sh
@@ -1,2 +1,7 @@
 docker build -t biostudies-mysql ../;
-docker-compose -f ../docker-compose.yml up -d
+docker run -d \
+  --name biostudies-mysql01 \
+  -e MYSQL_ROOT_PASSWORD=admin \
+  -e MYSQL_DATABASE=BioStudiesDev \
+  -p 3306:3306 \
+  biostudies-mysql;
