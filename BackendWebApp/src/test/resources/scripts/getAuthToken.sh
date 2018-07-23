ebb2b17749ff147bd959e53175267c7d3370fa3d
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-07-23 10:07:13 +0100

    [159111832] Get Auth Token Script
    Create Gradle task to get the authorisation token used by the application

---
+++ BackendWebApp/src/test/resources/scripts/getAuthToken.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+RESPONSE=$(curl -s \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"login":"admin_user@ebi.ac.uk", "password":"123456"}' \
+  http://localhost:8586/biostd/auth/signin);
+
+TOKEN=$(echo $RESPONSE | python -c 'import sys, json; print json.load(sys.stdin)["sessid"]');
+
+echo "Auth Token: $TOKEN";
