ece3a0b36ddc72ec9c003e11a341712ce2dbdf96
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-08-13 16:31:57 +0100

    [159493946] Improve Coverage
    - Add unit tests for missing cases in MagicFolderUtil
    - Add unit tests for FileManagerResource

--- BackendWebApp/src/test/java/uk/ac/ebi/biostd/webapp/application/security/service/MagicFolderUtilTest.java
+++ BackendWebApp/src/test/java/uk/ac/ebi/biostd/webapp/application/security/service/MagicFolderUtilTest.java
@@ -5,6 +5,9 @@ import static org.mockito.Mockito.when;
 import static uk.ac.ebi.biostd.webapp.application.security.service.MagicFolderUtil.USER_GROUP_DIR_PROP_NAME;
 
 import java.io.File;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -16,6 +19,8 @@ import uk.ac.ebi.biostd.webapp.application.configuration.ConfigProperties;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MagicFolderUtilTest {
+    private static final long TEST_ID = 123L;
+    private static final String TEST_SECRET = "abc-123";
 
     @ClassRule
     public static TemporaryFolder TEST_FOLDER = new TemporaryFolder();
@@ -55,6 +60,30 @@ public class MagicFolderUtilTest {
 
         File magicFolder = new File(parentFolder, "c-123-b40");
         assertThat(magicFolder).exists();
+    }
+
+    @Test
+    public void getUserMagicFolderPath() {
+        Path userMagicFolderPath = testInstance.getUserMagicFolderPath(TEST_ID, TEST_SECRET);
+        assertThat(userMagicFolderPath).isEqualTo(getExpectedPath(MagicFolderUtil.USER_FOLDER_PREFIX));
+    }
+
+    @Test
+    public void getGroupMagicFolderPath() {
+        Path userMagicFolderPath = testInstance.getGroupMagicFolderPath(TEST_ID, TEST_SECRET);
+        assertThat(userMagicFolderPath).isEqualTo(getExpectedPath(MagicFolderUtil.GROUP_FOLDER_PREFIX));
+    }
+
+    @Test(expected = NoSuchFileException.class)
+    public void createMagicFolderException() {
+        when(mockProperties.get(USER_GROUP_DIR_PROP_NAME)).thenReturn("/folder");
+        MagicFolderUtil faultyTestInstance = new MagicFolderUtil(mockProperties);
+
+        faultyTestInstance.createUserMagicFolder(TEST_ID, TEST_SECRET);
+    }
+
+    private Path getExpectedPath(String prefix) {
+        return Paths.get(String.format("%s/ab/c-123-%s%d", TEST_FOLDER.getRoot().getAbsolutePath(), prefix, TEST_ID));
     }
 }
 
