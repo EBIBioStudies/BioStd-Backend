e3ffe04f60bebe165042079d2103eda41aa3f85f
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-09-05 09:52:49 +0100

    Code review suggestions

--- Exporter/src/main/java/uk/ac/ebi/biostd/exporter/jobs/full/json/JsonSubmissionExporter.java
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/jobs/full/json/JsonSubmissionExporter.java
@@ -18,6 +18,7 @@ import org.easybatch.core.record.Record;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.biostd.exporter.jobs.common.base.QueueJob;
 import uk.ac.ebi.biostd.exporter.jobs.common.job.LogBatchListener;
+import uk.ac.ebi.biostd.exporter.jobs.full.configuration.FullExportFileProperties;
 import uk.ac.ebi.biostd.exporter.jobs.full.configuration.FullExportJobProperties;
 import uk.ac.ebi.biostd.exporter.jobs.full.job.FullExportJob;
 import uk.ac.ebi.biostd.exporter.model.ExecutionStats;
@@ -74,9 +75,8 @@ public final class JsonSubmissionExporter implements FullExportJob {
     }
 
     private String getFileName() {
-        return jobProperties.getAllSubmissions().getFilePath() +
-                jobProperties.getAllSubmissions().getFileName() +
-                EXTENSION;
+        FullExportFileProperties config = jobProperties.getAllSubmissions();
+
+        return config.getFilePath() + config.getFileName() + EXTENSION;
     }
 }
-
