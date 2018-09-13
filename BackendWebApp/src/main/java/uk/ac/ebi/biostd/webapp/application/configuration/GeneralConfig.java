943fc66dfd1ed46b113b5b237f6632f5c4dd420e
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2018-09-13 17:10:39 +0100

    pivotal-160487512: Magic folder creation at login and fix job - code review changes

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/configuration/GeneralConfig.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/configuration/GeneralConfig.java
@@ -10,9 +10,11 @@ import uk.ac.ebi.biostd.commons.files.MagicFolderUtil;
 @Configuration
 public class GeneralConfig {
 
+    private static final String USER_SYMLINK_PATH = "/usergroup/Users";
+
     @Bean
     public MagicFolderUtil magicFolderUtil(ConfigProperties configProperties) {
         return new MagicFolderUtil(configProperties.get(USER_GROUP_DIR_PROP_NAME),
-                configProperties.get(BIOSTUDY_BASE_DIR) + "/usergroup/Users");
+                configProperties.get(BIOSTUDY_BASE_DIR) + USER_SYMLINK_PATH);
     }
 }
