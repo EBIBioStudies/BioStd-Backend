147a48c1831d7288e0ba2f97a2b04f67c68ca26d
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2018-12-10 13:51:16 +0000

    Avoid Empty Properties List (#90)
    Avoid empty properties list since template query doesn't know how to handle them

--- Exporter/src/main/java/uk/ac/ebi/biostd/exporter/configuration/ExporterGeneralProperties.java
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/configuration/ExporterGeneralProperties.java
@@ -1,13 +1,17 @@
 package uk.ac.ebi.biostd.exporter.configuration;
 
 import java.util.List;
-import lombok.Data;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Data
+@Setter
 @Component
 @ConfigurationProperties(prefix = "jobs")
 public class ExporterGeneralProperties {
     private List<String> libFileStudies;
+
+    public List<String> getLibFileStudies() {
+        return libFileStudies.isEmpty() ? null : libFileStudies;
+    }
 }
