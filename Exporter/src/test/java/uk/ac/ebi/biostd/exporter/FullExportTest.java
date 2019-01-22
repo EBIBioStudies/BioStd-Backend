664668d6f989104a4a26209ac78965828de74905
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-01-22 16:18:12 +0000

    No Imaging Projects Integration Tests
    Integration tests to cover the scenario where neither LibFileStudies nor ImagingProjects are provided

--- Exporter/src/test/java/uk/ac/ebi/biostd/exporter/FullExportTest.java
+++ Exporter/src/test/java/uk/ac/ebi/biostd/exporter/FullExportTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 import static org.skyscreamer.jsonassert.JSONCompareMode.NON_EXTENSIBLE;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
 import com.google.common.collect.ImmutableList;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import javax.xml.transform.Source;
 import org.junit.Before;
@@ -26,6 +29,7 @@ import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
 import org.xmlunit.diff.Diff;
 import uk.ac.ebi.biostd.TestConfiguration;
+import uk.ac.ebi.biostd.exporter.configuration.ExporterGeneralProperties;
 import uk.ac.ebi.biostd.exporter.jobs.common.api.ExportPipeline;
 import uk.ac.ebi.biostd.exporter.jobs.full.FullExport;
 import uk.ac.ebi.biostd.exporter.jobs.full.FullJobJobsFactory;
@@ -44,6 +48,7 @@ import uk.ac.ebi.biostd.test.util.XmlAttributeFilter;
         "classpath:scripts/sql/init-full-export.sql",
         "classpath:scripts/sql/private_submission.sql",
         "classpath:scripts/sql/public_submission.sql"})
+@Sql(executionPhase = AFTER_TEST_METHOD, scripts = { "classpath:scripts/sql/drop_schema.sql", })
 public class FullExportTest extends BaseIntegrationTest {
     private static final String FULL_EXPORT_NAME = "studies";
     private static final String PUBLIC_ONLY_EXPORT_NAME = "publicOnlyStudies";
@@ -51,6 +56,8 @@ public class FullExportTest extends BaseIntegrationTest {
     private static final String EXPECTED_OUTPUT_PATH = "/test_files/";
     private static final String EXPECTED_FULL_XML_PATH = EXPECTED_OUTPUT_PATH + "full.xml";
     private static final String EXPECTED_FULL_JSON_PATH = EXPECTED_OUTPUT_PATH + "full.json";
+    private static final String EXPECTED_FULL_NO_LIB_FILES_XML_PATH = EXPECTED_OUTPUT_PATH + "fullNoLibFiles.xml";
+    private static final String EXPECTED_FULL_NO_LIB_FILES_JSON_PATH = EXPECTED_OUTPUT_PATH + "fullNoLibFiles.json";
     private static final String EXPECTED_PUBLIC_ONLY_JSON_PATH = EXPECTED_OUTPUT_PATH + "publicOnly.json";
     private static final String[] IGNORED_FIELDS = new String[]{"rtime", "ctime", "mtime", "@startTimeTS", "@endTimeTS",
             "@startTime", "@endTime", "@elapsedTime"};
@@ -62,6 +69,9 @@ public class FullExportTest extends BaseIntegrationTest {
 
     @MockBean
     private FullExportJobProperties exportProperties;
+
+    @MockBean
+    private ExporterGeneralProperties exporterGeneralProperties;
 
     @Mock
     private FullExportAllSubmissionsProperties allSubmissionsProperties;
@@ -79,6 +89,7 @@ public class FullExportTest extends BaseIntegrationTest {
     public void setup() {
         exportPipeline = new ExportPipeline(1, ImmutableList.of(fullExport), fullJobsFactory);
 
+        when(exporterGeneralProperties.getLibFileStudies()).thenReturn(Arrays.asList("S-EPMC2873748"));
         when(allSubmissionsProperties.getFilePath()).thenReturn(folder.getRoot().getAbsolutePath() + "/");
         when(allSubmissionsProperties.getFileName()).thenReturn(FULL_EXPORT_NAME);
         when(publicOnlySubmissionsProperties.getFilePath()).thenReturn(folder.getRoot().getAbsolutePath() + "/");
@@ -92,6 +103,16 @@ public class FullExportTest extends BaseIntegrationTest {
 
     @Test
     public void testFullExport() {
+        assertExportJobResults(EXPECTED_FULL_JSON_PATH, EXPECTED_FULL_XML_PATH);
+    }
+
+    @Test
+    public void testFullExportWithoutLibFileStudies() {
+        when(exporterGeneralProperties.getLibFileStudies()).thenReturn(Collections.emptyList());
+        assertExportJobResults(EXPECTED_FULL_NO_LIB_FILES_JSON_PATH, EXPECTED_FULL_NO_LIB_FILES_XML_PATH);
+    }
+
+    private void assertExportJobResults(String expectedFullJson, String expectedFullXml) {
         exportPipeline.execute();
 
         File[] files = folder.getRoot().listFiles();
@@ -99,12 +120,12 @@ public class FullExportTest extends BaseIntegrationTest {
 
         Arrays.sort(files, Comparator.comparing(File::getName));
         assertThatJsonFile(files[0], EXPECTED_PUBLIC_ONLY_JSON_PATH);
-        assertThatJsonFile(files[1], EXPECTED_FULL_JSON_PATH);
-        assertXmlFile(files[2]);
+        assertThatJsonFile(files[1], expectedFullJson);
+        assertXmlFile(files[2], expectedFullXml);
     }
 
-    private void assertXmlFile(File file) {
-        Source control = Input.fromFile(getResource(EXPECTED_FULL_XML_PATH).getAbsolutePath()).build();
+    private void assertXmlFile(File file, String expectedFilePath) {
+        Source control = Input.fromFile(getResource(expectedFilePath).getAbsolutePath()).build();
         String content = FileUtil.readFile(file.getAbsolutePath());
         Diff diff = DiffBuilder.compare(control).withTest(Input.fromString(content))
                 .ignoreWhitespace()
@@ -113,9 +134,9 @@ public class FullExportTest extends BaseIntegrationTest {
         assertThat(diff.hasDifferences()).isFalse();
     }
 
-    private void assertThatJsonFile(File file, String expectedFile) {
+    private void assertThatJsonFile(File file, String expectedFilePath) {
         String content = FileUtil.readFile(file.getAbsolutePath());
-        String expected = FileUtil.readFile(getResource(expectedFile).getAbsolutePath());
+        String expected = FileUtil.readFile(getResource(expectedFilePath).getAbsolutePath());
         assertEquals(expected, content, new JsonComparator(NON_EXTENSIBLE, IGNORED_FIELDS));
     }
 }
--- Exporter/src/test/java/uk/ac/ebi/biostd/exporter/StatsExportTest.java
+++ Exporter/src/test/java/uk/ac/ebi/biostd/exporter/StatsExportTest.java
@@ -2,12 +2,14 @@ package uk.ac.ebi.biostd.exporter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Files;
 import java.io.File;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Rule;
@@ -34,9 +36,11 @@ import uk.ac.ebi.biostd.exporter.jobs.stats.StatsProperties;
         "classpath:scripts/sql/init-full-export.sql",
         "classpath:scripts/sql/private_submission.sql",
         "classpath:scripts/sql/public_submission.sql"})
+@Sql(executionPhase = AFTER_TEST_METHOD, scripts = { "classpath:scripts/sql/drop_schema.sql", })
 public class StatsExportTest extends BaseIntegrationTest {
-    private static final String EXPECTED_STATS_1 = "\"S-EPMC2873748\",\"0\",\"1\",\"34984\",\"true\"";
-    private static final String EXPECTED_STATS_2 = "\"S-EPMC3343633\",\"0\",\"1\",\"9170139\",\"false\"";
+    private static final String EXPECTED_STATS_SUBMISSION_1_NO_IMG = "\"S-EPMC2873748\",\"0\",\"1\",\"34984\",\"false\"";
+    private static final String EXPECTED_STATS_SUBMISSION_1 = "\"S-EPMC2873748\",\"0\",\"1\",\"34984\",\"true\"";
+    private static final String EXPECTED_STATS_SUBMISSION_2 = "\"S-EPMC3343633\",\"0\",\"1\",\"9170139\",\"false\"";
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
@@ -68,13 +72,22 @@ public class StatsExportTest extends BaseIntegrationTest {
 
     @Test
     public void testStatsExport() throws Exception {
-        exportPipeline.execute();
-        File statsFile = new File(folder.getRoot().getAbsolutePath() + "/updates/stats.csv");
-        assertThat(statsFile).exists();
-        assertStats(Files.readLines(statsFile, Charset.defaultCharset()), EXPECTED_STATS_1, EXPECTED_STATS_2);
+        assertStatsJob(EXPECTED_STATS_SUBMISSION_1, EXPECTED_STATS_SUBMISSION_2);
     }
 
-    private void assertStats(List<String> stats, String... expectedStats) {
+    @Test
+    public void testStatsExportNoImagingProjects() throws Exception {
+        when(properties.getImagingProjects()).thenReturn(Collections.emptyList());
+        assertStatsJob(EXPECTED_STATS_SUBMISSION_1_NO_IMG, EXPECTED_STATS_SUBMISSION_2);
+    }
+
+    private void assertStatsJob(String... expectedStats) throws Exception {
+        exportPipeline.execute();
+
+        File statsFile = new File(folder.getRoot().getAbsolutePath() + "/updates/stats.csv");
+        assertThat(statsFile).exists();
+
+        List<String> stats = Files.readLines(statsFile, Charset.defaultCharset());
         assertThat(stats.size()).isEqualTo(expectedStats.length);
 
         for (int index = 0; index < stats.size(); index++) {
