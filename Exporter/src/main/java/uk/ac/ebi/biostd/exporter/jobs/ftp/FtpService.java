d5eb3f335bba70adba9c1711c1f680174171712f
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-03-21 10:41:39 +0000

    FTP Hard Links (#104)
    * FTP Hard Links
    
    - Create hard links for all files in public submissions
    - Add a test case for FTP hard links generation

--- Exporter/src/main/java/uk/ac/ebi/biostd/exporter/jobs/ftp/FtpService.java
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/jobs/ftp/FtpService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,31 +15,32 @@ import uk.ac.ebi.biostd.exporter.persistence.dao.SubmissionDao;
 @AllArgsConstructor
 @Slf4j
 public class FtpService {
-
     private final FtpPublisherProperties properties;
     private final SubmissionDao submissionDao;
     private final AtomicInteger count = new AtomicInteger(0);
 
     public void execute() {
-        submissionDao.getPublicSubmissionsPaths().forEach(this::createSymlink);
+        submissionDao.getPublicSubmissionsPaths().forEach(this::createLink);
         count.set(0);
     }
 
-    private void createSymlink(String submissionRelPath) {
-        File sourceFile = new File(properties.getBaseBioStudiesPath() + "/" + submissionRelPath + "/Files");
-        if (sourceFile.exists()) {
-            try {
-                File ftpFile = new File(properties.getBaseFtpPath() + "/" + submissionRelPath);
-                ftpFile.getParentFile().mkdirs();
+    private void createLink(String submissionRelPath) {
+        File submissionFiles = new File(properties.getBaseBioStudiesPath() + "/" + submissionRelPath + "/Files");
+        if (submissionFiles.exists() && submissionFiles.listFiles() != null) {
+            Stream.of(submissionFiles.listFiles()).parallel().forEach(file -> {
+                try {
+                    File ftpFile = new File(properties.getBaseFtpPath() + "/" + submissionRelPath + "/" + file.getName());
+                    ftpFile.getParentFile().mkdirs();
 
-                Path source = sourceFile.toPath();
-                Path ftp = ftpFile.toPath();
+                    Path source = file.toPath();
+                    Path ftp = ftpFile.toPath();
 
-                log.info("creating symbolic link {}, to {} in {}", count.getAndIncrement(), source, ftp);
-                Files.createSymbolicLink(ftp, source);
-            } catch (IOException e) {
-                log.error("Could not create symbolic link path", e);
-            }
+                    log.info("creating hard link {}, to {} in {}", count.getAndIncrement(), source, ftp);
+                    Files.createLink(ftp, source);
+                } catch (IOException e) {
+                    log.error("Could not create symbolic link path", e);
+                }
+            });
         }
     }
 }
