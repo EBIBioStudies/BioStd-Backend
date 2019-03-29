b23b396b26f6af23a9ff8a264be898f307066241
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2019-03-29 14:42:46 +0000

    code review changes

--- Exporter/src/main/java/uk/ac/ebi/biostd/exporter/model/Attribute.java
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/model/Attribute.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 
 @Data
 @AllArgsConstructor
@@ -25,24 +22,6 @@ public class Attribute {
 
     @JsonIgnore
     private Boolean reference;
-
-    @JsonIgnore
-    public List<Valqual> getValquals() {
-        if (StringUtils.isNotBlank(valueQualifierString)) {
-            List<Valqual> valquals = new ArrayList<>();
-            String[] values = valueQualifierString.split(";");
-            for (String value : values) {
-                String[] single = value.split("=");
-                Valqual valqual = single.length > 1 ? new Valqual(single[0], single[1]) : new Valqual(single[0], "");
-
-                valquals.add(valqual);
-            }
-
-            return valquals;
-        }
-
-        return null;
-    }
 
     public Attribute(String name, String value) {
         this.name = name;
