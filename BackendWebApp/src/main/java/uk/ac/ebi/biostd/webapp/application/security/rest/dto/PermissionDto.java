2637c021d4b2f81bbcd2181721fb4c6a4d65ee9e
Awais Athar <awais@ebi.ac.uk>
2019-04-09 11:52:39 +0100

    Fix null value when there are no files in the db (#108)
    * Updated secret key on login to the actual directory path
    Fixed login error messages
    
    * Added test for MagicFolderUtil.getUserMagicFolderRelativePath()
    
    * Fix null value returned when no file is present in the db
    
    * Refactored magicfolderutil code
    
    * Integrate UI and Submission Tool auth
    
    * Fixed unit tests

--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/security/rest/dto/PermissionDto.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/security/rest/dto/PermissionDto.java
@@ -3,8 +3,12 @@ package uk.ac.ebi.biostd.webapp.application.security.rest.dto;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Deprecated in favor of {@link LoginResponseDto}
+ */
 @Data
 @Builder
+@Deprecated
 public class PermissionDto {
 
     private String status;
--- BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/security/rest/mappers/PermissionMapper.java
+++ BackendWebApp/src/main/java/uk/ac/ebi/biostd/webapp/application/security/rest/mappers/PermissionMapper.java
@@ -1,12 +1,7 @@
 package uk.ac.ebi.biostd.webapp.application.security.rest.mappers;
 
-import static uk.ac.ebi.biostd.webapp.application.security.service.SecurityService.PUBLIC_ACCESS_TAG;
-
 import com.google.common.base.MoreObjects;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.biostd.commons.files.MagicFolderUtil;
@@ -20,20 +15,26 @@ import uk.ac.ebi.biostd.webapp.application.security.rest.dto.LoginResponseDto;
 import uk.ac.ebi.biostd.webapp.application.security.rest.dto.PermissionDto;
 import uk.ac.ebi.biostd.webapp.application.security.rest.model.UserData;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static uk.ac.ebi.biostd.webapp.application.security.service.SecurityService.PUBLIC_ACCESS_TAG;
+
 @Component
+@AllArgsConstructor
 public class PermissionMapper {
 
     private static final String STATUS_OK = "OK";
     private final MagicFolderUtil magicFolderUtil;
 
-    public PermissionMapper(MagicFolderUtil magicFolderUtil) {
-        this.magicFolderUtil = magicFolderUtil;
-    }
-
+    /**
+     * Deprecated in favor of {@link PermissionMapper#getLoginResponse(UserData userData)}
+     */
+    @Deprecated
     public Map<String, String> getPermissionMap(User user) {
         Map<String, String> accessInfo = new LinkedHashMap<>(7);
         accessInfo.put("Status", STATUS_OK);
-        accessInfo.put("Allow", getUserAllows(user));
+        accessInfo.put("Allow", StringUtils.join(getUserAllows(user),';'));
         accessInfo.put("Deny", StringUtils.EMPTY);
         accessInfo.put("Superuser", String.valueOf(user.isSuperuser()));
         accessInfo.put("Name", user.getFullName());
@@ -42,10 +43,14 @@ public class PermissionMapper {
         return accessInfo;
     }
 
+    /**
+     * Deprecated in favor of {@link PermissionMapper#getLoginResponse(UserData userData)}
+     */
+    @Deprecated
     public PermissionDto getPermissionDto(User user) {
         return PermissionDto.builder()
                 .status(STATUS_OK)
-                .allow(getUserAllows(user))
+                .allow( StringUtils.join(getUserAllows(user),';'))
                 .deny(StringUtils.EMPTY)
                 .superuser(user.isSuperuser())
                 .name(user.getFullName())
@@ -54,29 +59,34 @@ public class PermissionMapper {
                 .build();
     }
 
-    private String getUserAllows(User user) {
-        List<String> accessTags = user.getAccessPermissions()
+    private List<String> getUserAllows(User user) {
+        List<String> accessTags =  new ArrayList<>();
+        accessTags.add("~" + user.getEmail());
+        accessTags.add("#" + user.getId());
+        accessTags.add (PUBLIC_ACCESS_TAG);
+        accessTags.addAll( user.getAccessPermissions()
                 .stream()
                 .filter(permission -> permission.getAccessType().equals(AccessType.READ))
                 .map(AccessPermission::getAccessTag)
                 .map(AccessTag::getName)
-                .collect(Collectors.toList());
-        accessTags.add(PUBLIC_ACCESS_TAG);
-        return "~" + user.getEmail() + ";#" + user.getId() + ";" + String.join(";", accessTags);
+                .collect(Collectors.toList()));
+        return accessTags;
     }
 
     public LoginResponseDto getLoginResponse(UserData userData) {
         LoginResponseDto loginResponse = new LoginResponseDto();
         User user = userData.getUser();
 
+        loginResponse.setUsername(user.getLogin());
+        loginResponse.setFullname(user.getFullName());
         loginResponse.setEmail(user.getEmail());
         loginResponse.setSuperuser(String.valueOf(user.isSuperuser()));
-        loginResponse.setStatus(STATUS_OK);
-        loginResponse.setUsername(user.getFullName());
         loginResponse.setSessid(userData.getToken());
         loginResponse.setAux(getAuxInfo(user.getAuxProfileInfo()));
         loginResponse.setSecret(magicFolderUtil.getUserMagicFolderRelativePath(user.getId(),user.getSecret()).toString());
-
+        loginResponse.setAllow(getUserAllows(user));
+        loginResponse.setDeny(Collections.EMPTY_LIST); // TODO: Check why there is no deny list being tracked
+        loginResponse.setStatus(STATUS_OK);
         return loginResponse;
     }
 
