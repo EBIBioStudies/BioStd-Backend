edcb81aef1d2f36fac55e98b2dd9f2c0e0c3da72
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2019-04-30 13:59:08 +0100

    added script properties

---
+++ Documents/Deployment/Development.md
@@ -0,0 +1,4 @@
+#Bio-Studies Deployment
+Contains deployment configurations/scripts and documentation.
+
+- *scripts* folder contains copy of expected deploy script in dev environment.
--- Documents/Deployment/scripts/update.sh
+++ Documents/Deployment/scripts/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#
+# Kill current deployed instance and start deployment script.
+#
+# Parameters:
+#   1: deployment environment (dev|beta|prod).
+#   2: deploy configuration base path path.
+#
 
 cd $(dirname $0)
-PID=$(cat ../pid.txt)
+PID=$(cat $2/pid.txt)
 while $(kill -9 $PID 2>/dev/null); do sleep 1;done;
-sh run_backend.sh
+sh run_backend.sh $1 $2
