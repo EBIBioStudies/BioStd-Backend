d4344d02219279f6aab65d633eef2c06dbef5936
Juan Camilo Rada Mesa <jcamilorada@ebi.ac.uk>
2019-05-01 11:22:01 +0100

    added ci changes

--- BackendWebApp/ci/update.sh
+++ BackendWebApp/ci/update.sh
@@ -4,16 +4,18 @@
 # Kill current deployed instance and start deployment script.
 #
 # Parameters:
-#   1: deployment environment (dev|beta|prod).
-#   2: deploy configuration base path path.
-#   3: application port
-#   4: deploy jar artifact name, i.e. biostudy-20190430.jar
+#   1: deploy configuration base path path.
+#   2: application port
+#   3: deploy jar artifact name, i.e. biostudy-20190430.jar
 #
 cd $(dirname $0)
 
+netstat -nap | grep 8586
+
+
 ## kill current application
-PID=$(cat $2/pid.txt)
-while $(fuser -k $3/tcp>/dev/null); do sleep 1;done;
+#PID=$(netstat -antp 2>/dev/null -tlnp | awk '/:8586 */ {split($NF,a,"/"); print a[1]}')
+#while $(kill -9 ${PID} 2>/dev/null); do sleep 1;done;
 
 ## set specific application environment properties
 source environment.sh
@@ -21,6 +23,6 @@ source environment.sh
 ## deploy new version of application
 nohup $JAVA_HOME/bin/java \
     -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8001,suspend=n \
-    -jar "$2/$4.jar" \
-    --spring.config.location="$2/application.yml" \
-    --server.port=$3 >> logs.txt &
+    -jar "$1/$3" \
+    --spring.config.location="$1/application.yml" \
+    --server.port=$2 >> logs.txt &
