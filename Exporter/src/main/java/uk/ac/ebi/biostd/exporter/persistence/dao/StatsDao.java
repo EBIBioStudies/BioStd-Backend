faab26a760f81266316802f3a3a63731155cf1a4
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2020-06-10 11:43:16 +0100

    Pivotal ID # 172918296: Views Metadata Export
    Manage non existing stats in the SQL query instead of in the code

--- Exporter/src/main/java/uk/ac/ebi/biostd/exporter/persistence/dao/StatsDao.java
+++ Exporter/src/main/java/uk/ac/ebi/biostd/exporter/persistence/dao/StatsDao.java
@@ -1,9 +1,7 @@
 package uk.ac.ebi.biostd.exporter.persistence.dao;
 
 import com.google.common.collect.ImmutableMap;
-import java.util.Map;
 import lombok.AllArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.biostd.exporter.persistence.Queries;
@@ -17,15 +15,9 @@ public class StatsDao {
     private final NamedParameterJdbcTemplate template;
 
     public Integer getViews(String accNo) {
-        Integer views;
-        Map<String, String> params = ImmutableMap.of("accNo", accNo, "type", VIEW_TYPE);
-
-        try {
-            views = template.queryForObject(queries.getSubmissionStatsQuery(), params, Integer.class);
-        } catch (EmptyResultDataAccessException exception) {
-            views = 0;
-        }
-
-        return views;
+        return template.queryForObject(
+            queries.getSubmissionStatsQuery(),
+            ImmutableMap.of("accNo", accNo, "type", VIEW_TYPE),
+            Integer.class);
     }
 }
